6090b1a5853ef7238b50f96a02e89993b0497372
dappnodedev <diego@dappnode.io>
2024-07-11 23:54:19 +0200

    Improvements

--- beacon-chain/entrypoint.sh
+++ beacon-chain/entrypoint.sh
@@ -5,25 +5,30 @@ CHECKPOINT_SYNC_FLAG_1="--checkpoint-sync-url"
 CHECKPOINT_SYNC_FLAG_2="--genesis-beacon-api-url"
 MEVBOOST_FLAGS="--http-mev-relay"
 
+# shellcheck disable=SC1091 # Path is relative to the Dockerfile
+. /etc/profile
+
 handle_network() {
   case "$NETWORK" in
   "holesky")
-    set_beacon_config_by_network "${NETWORK}" "${SUPPORTED_NETWORKS}" "--holesky"
+    flags_to_set="--holesky"
     ;;
   "lukso")
-    set_beacon_config_by_network "${NETWORK}" "${SUPPORTED_NETWORKS}" "--chain-config-file=$LUKSO_CONFIG_PATH --contract-deployment-block=0 --bootstrap-node=enr:-MK4QJ-Bt9HATy4GQawPbDDTArtnt_phuWiVVoWKhS7-DSNjVzmGKBI9xKzpyRtpeCWd3qA9737FTdkKGDgtHfF4N-6GAYlzJCVRh2F0dG5ldHOIAAAAAAAAAACEZXRoMpA2ulfbQgAABP__________gmlkgnY0gmlwhCKTScGJc2VjcDI1NmsxoQJNpNUERqKhA8eDDC4tovG3a59NXVOW16JDFAWXoFFTEYhzeW5jbmV0cwCDdGNwgjLIg3VkcIIu4A","enr:-MK4QDOs4pISOkkYbVHnGYHC5EhYCsVzwguun6sFZjLTqrY6Kx_AoE-YyHvqBIHDUwyQqESC4-B3o6DigPQNfKpdhXiGAYgmPWCdh2F0dG5ldHOIAAAAAAAAAACEZXRoMpA2ulfbQgAABP__________gmlkgnY0gmlwhCIgwNOJc2VjcDI1NmsxoQNGVC8JPcsqsZPoohLP1ujAYpBfS0dBwiz4LeoUQ-k5OohzeW5jbmV0cwCDdGNwgjLIg3VkcIIu4A"
+    flags_to_set="--chain-config-file=$LUKSO_CONFIG_PATH --contract-deployment-block=0 --bootstrap-node=enr:-MK4QJ-Bt9HATy4GQawPbDDTArtnt_phuWiVVoWKhS7-DSNjVzmGKBI9xKzpyRtpeCWd3qA9737FTdkKGDgtHfF4N-6GAYlzJCVRh2F0dG5ldHOIAAAAAAAAAACEZXRoMpA2ulfbQgAABP__________gmlkgnY0gmlwhCKTScGJc2VjcDI1NmsxoQJNpNUERqKhA8eDDC4tovG3a59NXVOW16JDFAWXoFFTEYhzeW5jbmV0cwCDdGNwgjLIg3VkcIIu4A","enr:-MK4QDOs4pISOkkYbVHnGYHC5EhYCsVzwguun6sFZjLTqrY6Kx_AoE-YyHvqBIHDUwyQqESC4-B3o6DigPQNfKpdhXiGAYgmPWCdh2F0dG5ldHOIAAAAAAAAAACEZXRoMpA2ulfbQgAABP__________gmlkgnY0gmlwhCIgwNOJc2VjcDI1NmsxoQNGVC8JPcsqsZPoohLP1ujAYpBfS0dBwiz4LeoUQ-k5OohzeW5jbmV0cwCDdGNwgjLIg3VkcIIu4A"
     ;;
   "mainnet")
-    set_beacon_config_by_network "${NETWORK}" "${SUPPORTED_NETWORKS}" "--mainnet"
+    flags_to_set="--mainnet"
     ;;
   "sepolia")
-    set_beacon_config_by_network "${NETWORK}" "${SUPPORTED_NETWORKS}" "--sepolia"
+    flags_to_set="--sepolia"
     ;;
   *)
     echo "[ERROR - entrypoint] Unsupported network: $NETWORK"
     exit 1
     ;;
   esac
+
+  set_beacon_config_by_network "${NETWORK}" "${SUPPORTED_NETWORKS}" "$flags_to_set"
 }
 
 handle_checkpoint() {
@@ -45,19 +50,19 @@ run_beacon() {
   exec /beacon-chain \
     --accept-terms-of-use \
     --datadir=${DATA_DIR} \
-    --jwt-secret="${JWT_PATH}" \
+    --jwt-secret="${JWT_FILE_PATH}" \
     --execution-endpoint="${ENGINE_API_URL}" \
     --monitoring-host=0.0.0.0 \
     --grpc-gateway-host=0.0.0.0 \
-    --grpc-gateway-port="${VALIDATOR_PORT}" \
+    --grpc-gateway-port="${BEACON_API_PORT}" \
     --grpc-gateway-corsdomain="${CORSDOMAIN}" \
     --rpc-host=0.0.0.0 \
     --verbosity="${VERBOSITY}" \
     --p2p-tcp-port="${P2P_TCP_PORT}" \
     --p2p-udp-port="${P2P_UDP_PORT}" \
-    --p2p-max-peers="$MAX_PEERS" \
-    --min-sync-peers="$MIN_SYNC_PEERS" \
-    --subscribe-all-subnets="$SUBSCRIBE_ALL_SUBNETS" ${EXTRA_OPTS}
+    --p2p-max-peers="${MAX_PEERS}" \
+    --min-sync-peers="${MIN_SYNC_PEERS}" \
+    --subscribe-all-subnets="${SUBSCRIBE_ALL_SUBNETS}" ${EXTRA_OPTS}
 }
 
 validate_fee_recipient
--- validator/entrypoint.sh
+++ validator/entrypoint.sh
@@ -8,22 +8,25 @@ CLIENT="prysm"
 handle_network() {
     case "$NETWORK" in
     "holesky")
-        set_validator_config_by_network "${NETWORK}" "${SUPPORTED_NETWORKS}" "${CLIENT}" "--holesky"
+        flags_to_set="--holesky"
         ;;
     "lukso")
-        set_validator_config_by_network "${NETWORK}" "${SUPPORTED_NETWORKS}" "${CLIENT}" "--chain-config-file=$LUKSO_CONFIG_PATH"
+        flags_to_set="--chain-config-file=${LUKSO_CONFIG_PATH}"
         ;;
     "mainnet")
-        set_validator_config_by_network "${NETWORK}" "${SUPPORTED_NETWORKS}" "${CLIENT}" "--mainnet"
+        flags_to_set="--mainnet"
         ;;
     "sepolia")
-        set_validator_config_by_network "${NETWORK}" "${SUPPORTED_NETWORKS}" "${CLIENT}" "--sepolia"
+        flags_to_set="--sepolia"
         ;;
     *)
         echo "[ERROR - entrypoint] Unsupported network: $NETWORK"
         exit 1
         ;;
     esac
+
+    set_validator_config_by_network "${NETWORK}" "${SUPPORTED_NETWORKS}" "${CLIENT}" "$flags_to_set"
+
 }
 
 get_beacon_rpc_url() {
@@ -45,8 +48,8 @@ run_validator() {
         --beacon-rpc-gateway-provider="${BEACON_RPC_4000}" \
         --validators-external-signer-url="${WEB3SIGNER_API_URL}" \
         --grpc-gateway-host=0.0.0.0 \
-        --grpc-gateway-port="${VALIDATOR_PORT}" \
-        --grpc-gateway-corsdomain=http://0.0.0.0:"${VALIDATOR_PORT}" \
+        --grpc-gateway-port="${VALIDATOR_API_PORT}" \
+        --grpc-gateway-corsdomain=http://0.0.0.0:"${VALIDATOR_API_PORT}" \
         --graffiti="${GRAFFITI}" \
         --suggested-fee-recipient="${FEE_RECIPIENT}" \
         --verbosity="${VERBOSITY}" \
