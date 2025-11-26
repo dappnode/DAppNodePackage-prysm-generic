2099d6dbe75a8536bac9e20eb41c6a6001e42274
pablomendezroyo <41727368+pablomendezroyo@users.noreply.github.com>
2025-11-26 16:26:58 +0100

    fix rpc provider

--- validator/entrypoint.sh
+++ validator/entrypoint.sh
@@ -45,8 +45,8 @@ esac
 FLAGS="--datadir=$DATA_DIR \
     --wallet-dir=$WALLET_DIR \
     --monitoring-host=0.0.0.0 \
-    --beacon-rest-api-provider=${BEACON_DOMAIN}:3500 \
-    --beacon-rpc-provider=${BEACON_DOMAIN}:4000 \
+    --beacon-rest-api-provider=${BEACON_API_URL} \
+    --beacon-rpc-provider=http://${BEACON_DOMAIN}:4000 \
     --validators-external-signer-url=$SIGNER_API_URL \
     --http-host=0.0.0.0 \
     --http-port=$VALIDATOR_API_PORT \
