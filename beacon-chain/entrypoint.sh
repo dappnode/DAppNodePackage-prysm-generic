72b05c5d0713a650f63d66063ca8c44ac7234d53
hcastc00 <hcastc00@estudiantes.unileon.es>
2026-08-26 15:40:21 +0200

    chore: update upstream version to v7.1.2 and add LUKSO bootstrap nodes

--- beacon-chain/entrypoint.sh
+++ beacon-chain/entrypoint.sh
@@ -22,12 +22,15 @@ echo "${JWT_SECRET}" >"${JWT_FILE_PATH}"
 # To avoid failure for users who have added fee recipient to extra opts
 EXTRA_OPTS=$(add_flag_to_extra_opts_safely "${EXTRA_OPTS}" "--suggested-fee-recipient=${VALID_FEE_RECIPIENT}")
 
+# https://github.com/lukso-network/network-configs/blob/main/mainnet/prysm/prysm.yaml
+LUKSO_BOOTSTRAP_NODES="enr:-MK4QHcS3JeTtVjOuJyVXvO1E6XJWqiwmhLfodel6vARPI8ve_2q9vVn8LpIL964qBId7zGpSVKw6oOPAaRm2H7ywYiGAYmHDeBbh2F0dG5ldHOIAAAAAAAAAACEZXRoMpA2ulfbQgAABP__________gmlkgnY0gmlwhCIgwNOJc2VjcDI1NmsxoQNGVC8JPcsqsZPoohLP1ujAYpBfS0dBwiz4LeoUQ-k5OohzeW5jbmV0cwCDdGNwgjLIg3VkcIIu4A,enr:-Ly4QF7f-m7CiC1EwxmEa3VosxSUAeBaegcOek-J2DgqkB9EHyo5UOCZKAKuGN3pt3S28HVZy1PS6CGILRa8h25pZCsFh2F0dG5ldHOI__________-EZXRoMpCq3eQ4QgAABv__________gmlkgnY0gmlwhLJobp6Jc2VjcDI1NmsxoQJ88Tw_HsGhSdHp8AflKAJnt7VyHNDakc57AT6jdaBihIhzeW5jbmV0cw-DdGNwgiMog3VkcIIjKA,enr:-Jq4QABOsAAluqXVlLuAAWcMtYF__YTHcAWCChtvzXYa2GMuB5bEFaRnsoCuXUHTx4hxABFwwG29eQd3vH4GCZQbgdEBhGV0aDKQ3FGxEUIAAASkHwAAAAAAAIJpZIJ2NIJpcIQ5gLgeiXNlY3AyNTZrMaECWsDTDT1LTmpENdKPc6IbbViy5D1mahB4AJnPOCafYjKDdWRwgiMy"
+
 case "$NETWORK" in
 "hoodi")
   NETWORK_FLAGS="--hoodi"
   ;;
 "lukso")
-  NETWORK_FLAGS="--chain-config-file=$LUKSO_CHAIN_CONFIG_FILE_PATH --contract-deployment-block=0 --bootstrap-node=enr:-MK4QJ-Bt9HATy4GQawPbDDTArtnt_phuWiVVoWKhS7-DSNjVzmGKBI9xKzpyRtpeCWd3qA9737FTdkKGDgtHfF4N-6GAYlzJCVRh2F0dG5ldHOIAAAAAAAAAACEZXRoMpA2ulfbQgAABP__________gmlkgnY0gmlwhCKTScGJc2VjcDI1NmsxoQJNpNUERqKhA8eDDC4tovG3a59NXVOW16JDFAWXoFFTEYhzeW5jbmV0cwCDdGNwgjLIg3VkcIIu4A","enr:-MK4QDOs4pISOkkYbVHnGYHC5EhYCsVzwguun6sFZjLTqrY6Kx_AoE-YyHvqBIHDUwyQqESC4-B3o6DigPQNfKpdhXiGAYgmPWCdh2F0dG5ldHOIAAAAAAAAAACEZXRoMpA2ulfbQgAABP__________gmlkgnY0gmlwhCIgwNOJc2VjcDI1NmsxoQNGVC8JPcsqsZPoohLP1ujAYpBfS0dBwiz4LeoUQ-k5OohzeW5jbmV0cwCDdGNwgjLIg3VkcIIu4A"
+  NETWORK_FLAGS="--chain-config-file=$LUKSO_CHAIN_CONFIG_FILE_PATH --contract-deployment-block=0 --bootstrap-node=$LUKSO_BOOTSTRAP_NODES"
   ;;
 "mainnet")
   NETWORK_FLAGS="--mainnet"
